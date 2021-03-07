d88ddb2efbc5954f5d348a4379880ca91041ad7a
0-k <martin.t.klein@gmail.com>
2021-03-07 19:06:46 +0100

    adds first linear regression model

--- src/vscm.py
+++ src/vscm.py
@@ -1,1 +1,16 @@
 import pandas as pd
+from sklearn import linear_model
+from src.data import Data
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    data = Data().values
+    features = pd.DataFrame(data, columns=['Forcing', 'Irradiance', 'SO2', 'Aerosols'])
+    target = pd.DataFrame(data, columns=['Temperature'])
+    lm = linear_model.LinearRegression()
+    model = lm.fit(features, target)
+    predictions = lm.predict(features)
+    plt.plot(predictions)
+    plt.plot(target.values)
+    plt.show()
